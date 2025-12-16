ae79e8dcfc6bc5737e95ff724385265d83849efe
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-16 18:14:34 +0530

    [Swaraj]: added addition of two numbers program

---
+++ gcr-codebase/program-prerequisites/AddTwoNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class AddTwoNumbers{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("First Value :");
+        int val1 = sc.nextInt();
+        System.out.print("Second Value :");
+        int val2 = sc.nextInt();
+        System.out.print(val1 + val2);
+        
+    }
+}
