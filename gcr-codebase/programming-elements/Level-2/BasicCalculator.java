00d0c043ca6a893ff87e3a2585c08fae12251048
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-17 15:57:46 +0530

    [Swaraj]: added basic calculator program in level 2 folder

---
+++ gcr-codebase/programming-elements/Level-2/BasicCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class BasicCalculator{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+         
+
+        float number1 = input.nextInt();
+        float number2 = input.nextInt();
+        float addition = (number1 + number2);
+        float subtraction = (number1 - number2);
+        float multiplication = (number1 * number2);
+        float division = (number1 / number2);
+
+
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers " +number1 +" and "+ number2 +" is " + addition +", "+ subtraction +", "+ multiplication + ", and "+ division);  
+                                          }
+                }
