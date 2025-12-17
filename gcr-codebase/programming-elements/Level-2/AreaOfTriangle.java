c91c6e956487f040e0ad0d0dec860de32395e341
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-17 16:11:36 +0530

    [Swaraj]: added area of triangle program in level 2 folder

---
+++ gcr-codebase/programming-elements/Level-2/AreaOfTriangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class AreaOfTriangle{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+         
+
+        double base = input.nextInt();
+        double height = input.nextInt();
+
+        //Calculating the area of triangle.
+        double areaInCm = 0.5 * base * height ;
+        //converting it into sq inches.
+        double areaInInches = areaInCm / (2.54 * 2.54);
+
+
+        System.out.println("The Area of the triangle in sq in is " + areaInInches + " and sq cm is " + areaInCm);  
+                                          }
+                }
