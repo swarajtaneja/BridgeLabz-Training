8f0d027a5db0454555b8b3da74cdab490a346df5
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-17 15:15:01 +0530

    [Swaraj]: added fee discount with input program in level 1 folder

---
+++ gcr-codebase/programming-elements/Level-1/InputDiscountedFee.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class InputDiscountedFee{
+
+    public static void main(String[] args){
+        // Creating Scanner object to take input from user
+        Scanner input = new Scanner(System.in);
+         
+        //Taking Student Fee Details as input.
+        int fee = input.nextInt();
+
+        //Taking University discount Details as input.
+        double discountPercent = input.nextInt();
+
+        double discount = (fee * discountPercent) /100;
+        
+        System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + (fee - discount));  
+                                          }
+                }
