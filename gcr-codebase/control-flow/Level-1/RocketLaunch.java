02ccd434660fce02fb2ba1b85a5dc684b956c6e4
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-18 14:14:49 +0530

    [Swaraj]: added rocket launch counter program in control flow

---
+++ gcr-codebase/control-flow/Level-1/RocketLaunch.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RocketLaunch{
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Counter : ");
+        int counter = input.nextInt();
+
+        while(counter>0){
+            System.out.println(counter);
+            counter-=1;
+                         }
+                                          }
+                              }
