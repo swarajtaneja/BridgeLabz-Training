6af710cacee3e6383395f547d65cd8c8beff39e5
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-18 16:10:05 +0530

    [Swaraj]: modified factorial program in control flow level-1

--- gcr-codebase/control-flow/Level-1/Factorial.java
+++ gcr-codebase/control-flow/Level-1/Factorial.java
@@ -9,9 +9,15 @@ public class Factorial{
         System.out.print("Enter Number : ");
         int number = input.nextInt();
         
-        for(int i = number;i>0;i--){   
+        if(number > (-1)){
+            for(int i = number;i>0;i--){   
             factorial*=i;
                    }
         System.out.println("The factorial of number "+ number +" is "+ factorial );
+                          }
+        else{
+            System.out.println("The number " + number + " is not a natural number");
+            }
+        
                                           }
                               }
