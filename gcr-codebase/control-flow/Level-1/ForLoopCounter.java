e827e49de2e35c708915387d21be6a95bcd4dd7f
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-18 14:24:35 +0530

    [Swaraj]: added for-loop counter program in control flow level-1

---
+++ gcr-codebase/control-flow/Level-1/ForLoopCounter.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ForLoopCounter{
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Counter : ");
+        int counter = input.nextInt();
+
+        for(int i = counter;i>0;i--){
+            System.out.println(i);
+                         }
+                                          }
+                              }
