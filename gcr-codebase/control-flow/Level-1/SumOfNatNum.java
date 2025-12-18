44d4604ae7d25a5b51e35cee45288efbf387a540
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-18 15:27:21 +0530

    [Swaraj]: added natural number sum comparison program in control flow level-1

---
+++ gcr-codebase/control-flow/Level-1/SumOfNatNum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumOfNatNum{
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int sum = 0, number, n;
+
+        System.out.print("Enter Number : ");
+        number = input.nextInt();
+        n = number*(number + 1) / 2;     
+        
+        while(number>0){   
+            sum+=number;
+            number--;
+                   }
+        if(sum == n){
+         System.out.println("The result From both computations was correct");
+                    }
+                                          }
+                              }
