f435505556b08669e06d6e108b313d30d4f4dd5d
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-18 13:12:13 +0530

    [Swaraj]: added number check program in control flow

---
+++ gcr-codebase/control-flow/Level-1/NumberCheck.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NumberCheck{
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Number : ");
+        int number = input.nextInt();
+
+        if(number > 0){
+            System.out.println("positive");
+                                                  }
+        else if (number < 0){
+            System.out.println("negative");
+            }
+        else{
+            System.out.println("zero");
+            }                                          }
+                              }
