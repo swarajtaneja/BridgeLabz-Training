a7aecefb6e08789610b16145152c77e6f7184dd8
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-19 11:15:18 +0530

    [Swaraj]: added multiples using for-loop program in control flow level-2

---
+++ gcr-codebase/control-flow/Level-2/MultipleUsingForLoop.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MultipleUsingForLoop{
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+
+        System.out.print("Enter Positive Number : ");
+        int number = input.nextInt();
+
+        if(number>0 && number<100){
+              System.out.print("The Multiples of Number "+number+" is : ");
+              
+              for(int i=100;i>1;i--){
+                  if(number%i==0){
+                      System.out.print(i+"  ");
+                                  }
+                  else{continue;}
+                                          }    
+                                   }
+        else{
+            System.out.println("Not a positive integer");
+             }
+
+                                        }
+                         }
