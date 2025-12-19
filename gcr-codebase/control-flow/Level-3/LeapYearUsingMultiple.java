83f47818e3812e18a5440e0fe7a73339a5d3575e
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-19 12:09:45 +0530

    [Swaraj]: added leap year using multiple if-else program in control flow level-3

---
+++ gcr-codebase/control-flow/Level-3/LeapYearUsingMultiple.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LeapYearUsing{
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int i=1;
+
+        System.out.print("Enter Year : ");
+        int year = input.nextInt();
+
+        if(year>=1582){
+             if (year % 4==0){
+                 if (year % 100==0){
+                     if(year % 400==0){
+                          System.out.println("The year "+year+" is a leap year");
+                                       } 
+                     else{System.out.println("The year "+year+" is not a leap year");}
+                                   }
+                 else{System.out.println("The year "+year+" is a leap year");}
+                             } 
+               else{System.out.println("The year "+year+" is not a leap year");}   
+                      }
+        else{
+            System.out.println("Outside Gregorian calendar");
+             }
+
+                                        }
+                         }
