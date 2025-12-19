a6be74882f7a2796698f55ab7ea6e0914ba9f689
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-19 12:11:11 +0530

    [Swaraj]: updated leap year using multiple if-else program in control flow level-3

--- gcr-codebase/control-flow/Level-3/LeapYearUsingMultiple.java
+++ gcr-codebase/control-flow/Level-3/LeapYearUsingMultiple.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LeapYearUsing{
+public class LeapYearUsingMultiple{
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
