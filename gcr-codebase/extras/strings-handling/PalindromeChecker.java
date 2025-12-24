04f7f388a4db803a0d26f8d0633461534cc5ec51
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-24 12:09:54 +0530

    [Swaraj]: added palindrome program in extras strings handling

---
+++ gcr-codebase/extras/strings-handling/PalindromeChecker.java
@@ -0,0 +1,22 @@
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        String str = "madam";
+        boolean isPalindrome = true;
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+        if (isPalindrome) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+}
