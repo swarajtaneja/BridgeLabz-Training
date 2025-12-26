d33fc94bdecfb1ffd11777c005e1d43bdb32b28a
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-26 22:10:19 +0530

    [Swaraj] :  Added convert sorted array program in leetcode arrays day-2

---
+++ leet-code-codebase/arrays/day-2/ConvertSortedArray.java
@@ -0,0 +1,19 @@
+public class ConvertSortedArray {
+    public TreeNode sortedArrayToBST(int[] nums) {
+      return buildBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildBST(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+
+        root.left = buildBST(nums, left, mid - 1);
+        root.right = buildBST(nums, mid + 1, right);
+
+        return root;
+    }
+}
