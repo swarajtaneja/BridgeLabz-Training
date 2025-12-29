345c94c2b942023d2749891a9998875283dbc993
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-29 08:10:56 +0530

    [Swaraj]: Added container with water program in leetcode ararys day-5

---
+++ leet-code-codebase/arrays/day-5/ContainerWithWater.java
@@ -0,0 +1,11 @@
+public class ContainerWithWater {
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1, max = 0;
+        while (l < r) {
+            max = Math.max(max, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) l++;
+            else r--;
+        }
+        return max;
+    }
+}
