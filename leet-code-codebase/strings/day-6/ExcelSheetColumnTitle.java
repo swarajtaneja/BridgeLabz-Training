f7d997702116caa7d906d90c71851f3d944393c2
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-29 18:24:24 +0530

    [Swaraj] : Added Excel sheet column title program in leetcode day-6

---
+++ leet-code-codebase/strings/day-6/ExcelSheetColumnTitle.java
@@ -0,0 +1,12 @@
+public class ExcelSheetColumnTitle{
+    public String convertToTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+        while (columnNumber > 0) {
+            columnNumber--; 
+            result.append((char) ('A' + columnNumber % 26));
+            columnNumber /= 26;
+        }
+        return result.reverse().toString();
+    }
+}
+
