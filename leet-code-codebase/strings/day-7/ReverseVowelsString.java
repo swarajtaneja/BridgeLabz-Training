ad1766737bf713cb9a90a704efae84304387e956
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-30 15:13:05 +0530

    [Swaraj]: Added reverse vowels string program in day-7 of leetcode topic strings

---
+++ leet-code-codebase/strings/day-7/ReverseVowelsString.java
@@ -0,0 +1,20 @@
+public class ReverseVowelsString {
+    public String reverseVowels(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0, right = arr.length - 1;
+        String vowels = "aeiouAEIOU";
+
+        while (left < right) {
+            while (left < right && vowels.indexOf(arr[left]) == -1) left++;
+            while (left < right && vowels.indexOf(arr[right]) == -1) right--;
+
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+        return new String(arr);
+    }
+}
