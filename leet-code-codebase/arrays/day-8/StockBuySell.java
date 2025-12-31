959b60ad9b7c17362b053bc9cbe7a291563bd1f9
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-31 21:08:03 +0530

    [Swaraj] : Added stock buy sell program in leetcode arrays day-8

---
+++ leet-code-codebase/arrays/day-8/StockBuySell.java
@@ -0,0 +1,12 @@
+public class StockBuySell {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+}
