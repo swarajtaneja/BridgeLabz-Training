7add85e8d5b3a246e24ba506ddd02751260412b9
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-31 21:09:18 +0530

    [Swaraj] : Added contains duplicates 2 program in leetcode arrays day-8

---
+++ leet-code-codebase/arrays/day-8/ContainsDuplicates2.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class ConatinsDuplicates2 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
