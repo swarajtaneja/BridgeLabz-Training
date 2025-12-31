ac78db05362fa361977301ee75e86cbfa5a2d913
swarajtaneja <swaraj97713473431@gmail.com>
2025-12-31 21:09:08 +0530

    [Swaraj] : Added contains duplicates program in leetcode arrays day-8

---
+++ leet-code-codebase/arrays/day-8/ContainsDuplicates.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class ConatinsDuplicates {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
