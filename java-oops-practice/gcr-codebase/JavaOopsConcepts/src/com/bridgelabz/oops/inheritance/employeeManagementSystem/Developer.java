2a16bc878c81866cb769616a9ab8cca1a97dfe13
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-01 14:21:26 +0530

    [Swaraj] : Added developer file in inheritance employee management system package

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/inheritance/employeeManagementSystem/Developer.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.oops.inheritance.employeeManagementSystem;
+
+public class Developer extends Employee {
+	private String programmingLanguage;
+	public Developer(String name,int id , double salary,String programmingLanguage) {
+		super(name,id,salary);
+		this.programmingLanguage=programmingLanguage;
+	}
+	@Override
+	public void displayDetails() {
+		System.out.println("Employee Post : Develpoer");
+		super.displayDetails();
+		System.out.println("Programming Language : "+programmingLanguage);
+	}
+
+}
