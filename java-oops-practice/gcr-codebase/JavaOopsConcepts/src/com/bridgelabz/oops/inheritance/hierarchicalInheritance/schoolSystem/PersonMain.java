8898df5b6c50d426c88a02c4eafeb076536921fa
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-01 18:25:10 +0530

    [Swaraj] : Added person main file in hierarchialInheritance school system

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/inheritance/hierarchicalInheritance/schoolSystem/PersonMain.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.oops.inheritance.hierarchicalInheritance.schoolSystem;
+
+public class PersonMain {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Teacher t1 = new Teacher("Raj", 33, "Physics");
+		t1.displayRole();
+		System.out.println();
+		
+		Student s1 = new Student("Ashish", 17, 'C');
+		s1.displayRole();
+		System.out.println();
+		
+		Staff s2 = new Staff("Swaraj", 22);
+		s2.displayRole();
+	}
+
+}
