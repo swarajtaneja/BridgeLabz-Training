8c8377029e50860e282320d1c42a19321ea713f1
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-01 14:21:44 +0530

    [Swaraj] : Added manager file in inheritance employee management system package

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/inheritance/employeeManagementSystem/Manager.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.oops.inheritance.employeeManagementSystem;
+
+public class Manager extends Employee {
+	private int teamSize;
+	public Manager(String name,int id ,double salary,int teamSize) {
+		super(name,id,salary);
+		this.teamSize=teamSize;
+	}
+	@Override
+	public void displayDetails() {
+		System.out.println("Employee Post : Manager");
+		super.displayDetails();
+		System.out.println("Team Size : "+teamSize);
+	}
+
+}
