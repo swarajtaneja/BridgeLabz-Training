00af14764277104c2711299a5e00ca908c376ce9
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-02 12:05:50 +0530

    [Swaraj] : Added bike file in encapsulation ride application

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/encapsulation/rideapplication/Bike.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.oops.encapsulation.rideapplication;
+
+public class Bike extends Vehicle implements GPS {
+
+    private String location;
+
+    public Bike(int vehicleId, String driverName, double ratePerKm) {
+        super(vehicleId, driverName, ratePerKm);
+    }
+
+    @Override
+    public double calculateFare(double distance) {
+        return distance * getRatePerKm() * 0.8; // Cheaper rate
+    }
+
+    @Override
+    public String getCurrentLocation() {
+        return location;
+    }
+
+    @Override
+    public void updateLocation(String newLocation) {
+        this.location = newLocation;
+    }
+}
