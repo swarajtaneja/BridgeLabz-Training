bdd2e1fd15c6a9031747f80b6ffa3601b02cce1a
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-02 11:51:22 +0530

    [Swaraj] : Added product file in encapsulation ecomplatform

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/encapsulation/ecomplatform/Product.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.oops.encapsulation.ecomplatform;
+
+public abstract class Product {
+
+    private int productId;
+    private String name;
+    protected double price;
+
+    public Product(int productId, String name, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Encapsulation: Getters & Setters
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Abstract method
+    public abstract double calculateDiscount();
+}
