390b3a3bdadc6c89b7b63fb9b36921ea8327eac3
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-02 12:06:05 +0530

    [Swaraj] : Added Car file in encapsulation ride application

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/encapsulation/rideapplication/Car.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.oops.encapsulation.rideapplication;
+
+public class Car extends Vehicle implements GPS {
+
+    private String location;
+
+    public Car(int vehicleId, String driverName, double ratePerKm) {
+        super(vehicleId, driverName, ratePerKm);
+    }
+
+    @Override
+    public double calculateFare(double distance) {
+        return distance * getRatePerKm(); // Standard rate
+    }
+
+    @Override
+    public String getCurrentLocation() {
+        return location;
+    }
+
+    @Override
+    public void updateLocation(String newLocation) {
+        this.location = newLocation;
+    }
+}
