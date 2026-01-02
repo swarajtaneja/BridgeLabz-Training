9c202c60963deb9a493644d8380e683a4bda1dee
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-02 12:07:47 +0530

    [Swaraj] : Added bike file in encapsulation vehicle system

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/encapsulation/vehiclesystem/Bike.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.oops.encapsulation.vehiclesystem;
+
+public class Bike extends Vehicle implements Insurable {
+
+    private String insurancePolicyNumber;
+
+    public Bike(String vehicleNumber, double rentalRate, String policyNumber) {
+        super(vehicleNumber, "Bike", rentalRate);
+        this.insurancePolicyNumber = policyNumber;
+    }
+
+    private String getInsurancePolicyNumber() {
+        return insurancePolicyNumber;
+    }
+
+    @Override
+    public double calculateRentalCost(int days) {
+        return rentalRate * days;
+    }
+
+    @Override
+    public double calculateInsurance() {
+        return 200;
+    }
+
+    @Override
+    public String getInsuranceDetails() {
+        return "Bike Insurance (Policy Protected)";
+    }
+}
