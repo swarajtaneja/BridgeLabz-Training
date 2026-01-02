b8198d1f71271bebcee19fb5e29381205c122630
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-02 11:46:25 +0530

    [Swaraj] : Added bank account file in encapsulation banking system

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/encapsulation/bankingsystem/BankAccount.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.oops.encapsulation.bankingsystem;
+
+public abstract class BankAccount {
+
+    private String accountNumber;
+    private String holderName;
+    protected double balance;
+
+    public BankAccount(String accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    // Encapsulation: Getters
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // Concrete methods
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    // Abstract method
+    public abstract double calculateInterest();
+}
