0b35cfcdcf0d971bc30203bc59c711ed7208c54c
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-02 11:55:11 +0530

    [Swaraj] : Added full time employee file in encapsulation employee management

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/encapsulation/employeemanagement/FullTimeEmployee.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.oops.encapsulation.employeemanagement;
+
+public class FullTimeEmployee extends Employee implements Department {
+
+    private String department;
+
+    public FullTimeEmployee(int employeeId, String name, double baseSalary) {
+        super(employeeId, name, baseSalary);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return baseSalary; 
+    }
+
+    @Override
+    public void assignDepartment(String departmentName) {
+        this.department = departmentName;
+    }
+
+    @Override
+    public String getDepartmentDetails() {
+        return department;
+    }
+}
