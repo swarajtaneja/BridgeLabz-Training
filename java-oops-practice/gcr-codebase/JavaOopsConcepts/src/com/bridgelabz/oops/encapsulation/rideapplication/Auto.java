22c3701849a5a5a0544cf91586961261a26b90d5
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-02 12:05:38 +0530

    [Swaraj] : Added auto file in encapsulation ride application

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/encapsulation/rideapplication/Auto.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.oops.encapsulation.rideapplication;
+
+public class Auto extends Vehicle implements GPS {
+
+    private String location;
+
+    public Auto(int vehicleId, String driverName, double ratePerKm) {
+        super(vehicleId, driverName, ratePerKm);
+    }
+
+    @Override
+    public double calculateFare(double distance) {
+        return distance * getRatePerKm() + 20; // Base charge
+    }
+
+    @Override
+    public String getCurrentLocation() {
+        return location;
+    }
+
+    @Override
+    public void updateLocation(String newLocation) {
+        this.location = newLocation;
+    }
+}
