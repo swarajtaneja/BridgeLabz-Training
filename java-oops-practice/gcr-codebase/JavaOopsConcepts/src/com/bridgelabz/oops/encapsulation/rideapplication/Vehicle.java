26307cc20c2f484d3ef631c64558db90ab3591cb
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-02 12:06:36 +0530

    [Swaraj] : Added vehicle file in encapsulation ride application

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/encapsulation/rideapplication/Vehicle.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.oops.encapsulation.rideapplication;
+
+public abstract class Vehicle {
+    private int vehicleId;
+    private String driverName;
+    private double ratePerKm;
+
+    public Vehicle(int vehicleId, String driverName, double ratePerKm) {
+        this.vehicleId = vehicleId;
+        this.driverName = driverName;
+        this.ratePerKm = ratePerKm;
+    }
+
+    // Abstract method
+    public abstract double calculateFare(double distance);
+
+    // Concrete method
+    public void getVehicleDetails() {
+        System.out.println("Vehicle ID: " + vehicleId);
+        System.out.println("Driver Name: " + driverName);
+        System.out.println("Rate per Km: " + ratePerKm);
+    }
+
+    // Encapsulation: controlled access
+    protected double getRatePerKm() {
+        return ratePerKm;
+    }
+}
