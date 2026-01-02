dfba6bb67e6c2cafd4c6991f9017329c8b09b8b9
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-02 12:09:16 +0530

    [Swaraj] : Added vehicle file in encapsulation vehicle system

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/encapsulation/vehiclesystem/Vehicle.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.oops.encapsulation.vehiclesystem;
+
+public abstract class Vehicle {
+
+    private String vehicleNumber;
+    private String type;
+    protected double rentalRate;
+
+    public Vehicle(String vehicleNumber, String type, double rentalRate) {
+        this.vehicleNumber = vehicleNumber;
+        this.type = type;
+        this.rentalRate = rentalRate;
+    }
+
+    // Encapsulation: Getters & Setters
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public void setVehicleNumber(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getRentalRate() {
+        return rentalRate;
+    }
+
+    // Abstract method
+    public abstract double calculateRentalCost(int days);
+}
