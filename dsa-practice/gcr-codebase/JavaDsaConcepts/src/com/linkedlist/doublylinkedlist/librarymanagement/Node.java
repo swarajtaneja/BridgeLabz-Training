54dbc5314e7a6047a24c51c2769cf7400b7c9705
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-03 20:43:44 +0530

    [Swaraj] : Added node file in doublylinkedlist library management

---
+++ dsa-practice/gcr-codebase/JavaDsaConcepts/src/com/linkedlist/doublylinkedlist/librarymanagement/Node.java
@@ -0,0 +1,22 @@
+package com.linkedlist.doublylinkedlist.librarymanagement;
+
+public class Node {
+	 int bookId;
+	 String title;
+	 String author;
+	 String genre;
+	 boolean isAvailable;
+
+	 Node next;
+	 Node prev;
+
+	 Node(int bookId, String title, String author, String genre, boolean isAvailable) {
+		 this.bookId = bookId;
+		 this.title = title;
+		 this.author = author;
+		 this.genre = genre;
+		 this.isAvailable = isAvailable;
+		 this.next = null;
+		 this.prev = null;
+	 }
+}
