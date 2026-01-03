2927e2ce6254225914fa072d51799e6813bcae37
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-03 20:41:56 +0530

    [Swaraj] : Added node file in doublylinked list text editor

---
+++ dsa-practice/gcr-codebase/JavaDsaConcepts/src/com/linkedlist/doublylinkedlist/texteditor/Node.java
@@ -0,0 +1,13 @@
+package com.linkedlist.doublylinkedlist.texteditor;
+
+public class Node {
+	 String content;
+	 Node prev;
+	 Node next;
+
+	 Node(String content) {
+		 this.content = content;
+		 this.prev = null;
+		 this.next = null;
+	 }
+}
