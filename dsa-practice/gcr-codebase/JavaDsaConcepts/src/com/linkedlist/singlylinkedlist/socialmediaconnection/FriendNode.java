a042066f292501dc3a4e5ab54456abc07163b62b
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-03 20:38:46 +0530

    [Swaraj] : Added friend node file in singlylinkedlist social media connection

---
+++ dsa-practice/gcr-codebase/JavaDsaConcepts/src/com/linkedlist/singlylinkedlist/socialmediaconnection/FriendNode.java
@@ -0,0 +1,11 @@
+package com.linkedlist.singlylinkedlist.socialmediaconnection;
+
+public class FriendNode {
+	int friendId;
+	FriendNode next;
+
+	FriendNode(int friendId) {
+		this.friendId = friendId;
+		this.next = null;
+	}
+}
