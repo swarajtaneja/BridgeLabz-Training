3ff59255fb3dcc342bcae033e87a988f470f3a72
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-05 16:21:49 +0530

    [Swaraj] : Added product file in scenario based swiftcart

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/swiftcart/Product.java
@@ -0,0 +1,28 @@
+package com.swiftcart;
+
+public abstract class Product {
+    private String name;
+    private double price;
+    private String category;
+
+    public Product(String name, double price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    // Polymorphic discount method
+    public abstract double getDiscount();
+}
