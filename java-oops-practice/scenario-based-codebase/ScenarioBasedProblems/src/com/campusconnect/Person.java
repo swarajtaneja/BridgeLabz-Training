4fc59a49b4ff85cb44e65bfab393c549b97dbbee
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-05 16:18:23 +0530

    [Swaraj] : Added Person file in scenario based campusconnect

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/campusconnect/Person.java
@@ -0,0 +1,16 @@
+package com.campusconnect;
+
+public abstract class Person {
+    protected int id;
+    protected String name;
+    protected String email;
+
+    public Person(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Polymorphism
+    public abstract void printDetails();
+}
