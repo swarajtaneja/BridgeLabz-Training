0baeed6805e3cb3eac6beec710533670b5da13a8
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-06 21:23:49 +0530

    [Swaraj] : Added petrolpump file in stackqueue circulartour

---
+++ dsa-practice/gcr-codebase/JavaDsaConcepts/src/com/stackandqueue/circulartour/PetrolPump.java
@@ -0,0 +1,11 @@
+package com.stackandqueue.circulartour;
+
+public class PetrolPump {
+	int petrol;
+    int distance;
+
+    public PetrolPump(int petrol, int distance) {
+        this.petrol = petrol;
+        this.distance = distance;
+    }
+}
