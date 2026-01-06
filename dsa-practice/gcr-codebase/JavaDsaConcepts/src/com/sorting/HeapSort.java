77d4c9f525bb767f3d9f3807863dce94be87766e
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-06 21:31:24 +0530

    [Swaraj] : Added heap sort file in sorting

---
+++ dsa-practice/gcr-codebase/JavaDsaConcepts/src/com/sorting/HeapSort.java
@@ -0,0 +1,53 @@
+package com.sorting;
+
+public class HeapSort {
+	
+    static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    static void heapSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] salaryDemands = {55000, 72000, 45000, 90000, 60000};
+
+        heapSort(salaryDemands);
+
+        System.out.println("Sorted Salary Demands:");
+        for (int salary : salaryDemands) {
+            System.out.print(salary + " ");
+        }
+    }
+}
