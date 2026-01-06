c2fc50a6ecdd2f648826182fbf5fb0ea3bae3670
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-06 12:14:20 +0530

    [Swaraj] : Added stack node file in sliding window

---
+++ dsa-practice/gcr-codebase/JavaDsaConcepts/src/com/stackandqueue/slidingwindow/StackNode.java
@@ -0,0 +1,13 @@
+package com.stackandqueue.slidingwindow;
+
+public class StackNode {
+
+		
+		public int index;
+		public StackNode next;
+		public StackNode prev;
+		
+		public StackNode(int index) {
+			this.index = index;
+		}
+}
