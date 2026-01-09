87df527f5d6d19834bbe33a276de96f08cd91d39
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-09 16:34:20 +0530

    [Swaraj] : Added instructor file in scenario based skillforge

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/skillforge/Instructor.java
@@ -0,0 +1,12 @@
+package com.skillforge;
+
+public class Instructor extends User {
+
+    public Instructor(String name, String email) {
+        super(name, email);
+    }
+
+    public void uploadCourse(Course course) {
+        System.out.println("Course uploaded: " + course.getTitle());
+    }
+}
