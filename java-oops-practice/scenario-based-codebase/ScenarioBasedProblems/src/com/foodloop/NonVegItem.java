f98f6f613156a2cff61da63ddab7a283346d5a64
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-09 16:30:39 +0530

    [Swaraj] : Added Non VegItem file in scenario based foodloop

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/foodloop/NonVegItem.java
@@ -0,0 +1,8 @@
+package com.foodloop;
+
+public class NonVegItem extends FoodItem {
+
+    public NonVegItem(String name, double price, int stock) {
+        super(name, "Non-Veg", price, stock);
+    }
+}
