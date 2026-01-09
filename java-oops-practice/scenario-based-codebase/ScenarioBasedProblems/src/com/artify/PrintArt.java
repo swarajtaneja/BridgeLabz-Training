77790d906896ba1f9dae2c0928a0e5186e8d0d1f
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-09 16:25:43 +0530

    [Swaraj] : Added print art file in scenario based artify

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/artify/PrintArt.java
@@ -0,0 +1,23 @@
+package com.artify;
+
+public class PrintArt extends ArtWork{
+	public PrintArt(String title, String artist, double price) {
+		super(title,artist,price,"License : ",true);
+	}
+	@Override
+	public void purchase(User user) {
+		if (user.getWalletBalance() >= price) {
+            user.setWalletBalance(user.getWalletBalance() - price);
+            System.out.println(user.getName() + " purchased print art: " + getTitle());
+        } else {
+            System.out.println("Insufficient balance!");
+        }
+		
+	}
+	@Override
+	public void license() {
+		System.out.println("Print Art License : Limited Copies");
+	}
+
+
+}
