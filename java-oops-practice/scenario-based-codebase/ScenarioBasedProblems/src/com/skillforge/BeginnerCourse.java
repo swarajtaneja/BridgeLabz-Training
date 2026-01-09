5d6ab8d578d23ce17e1e6b42b3d5ab61b7071da0
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-09 16:32:35 +0530

    [Swaraj] : Added BeginnerCourse file in scenario based skillforge

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/skillforge/BeginnerCourse.java
@@ -0,0 +1,8 @@
+package com.skillforge;
+
+public class BeginnerCourse extends Course {
+
+    public BeginnerCourse(String title, Instructor instructor) {
+        super(title, instructor);
+    }
+}
