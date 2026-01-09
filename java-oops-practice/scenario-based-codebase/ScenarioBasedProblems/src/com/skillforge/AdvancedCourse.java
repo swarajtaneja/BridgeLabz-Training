49aadc35922ce41247a954698a107069a1d99c9d
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-09 16:32:21 +0530

    [Swaraj] : Added advancedCourse file in scenario based skillforge

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/skillforge/AdvancedCourse.java
@@ -0,0 +1,8 @@
+package com.skillforge;
+
+public class AdvancedCourse extends Course {
+
+    public AdvancedCourse(String title, Instructor instructor, String[] modules) {
+        super(title, instructor, modules);
+    }
+}
