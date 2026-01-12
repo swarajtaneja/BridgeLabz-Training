3ef192fe22f037c582e00136b95ed6e8a1e43e2e
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-12 13:13:30 +0530

    [Swaraj] : Added customer file in scenario based callcenter

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/callcenter/Customer.java
@@ -0,0 +1,11 @@
+package com.callcenter;
+
+public class Customer {
+    String name;
+    boolean isVIP;
+
+    public Customer(String name, boolean isVIP) {
+        this.name = name;
+        this.isVIP = isVIP;
+    }
+}
