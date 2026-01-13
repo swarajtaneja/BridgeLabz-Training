24ca8ed361acb20b4b0b5a89420e2a11623db647
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-13 15:05:21 +0530

    [Swaraj] : Added budget file in scenarionbased budgetwise

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/budgetwise/Budget.java
@@ -0,0 +1,34 @@
+package com.budgetwise;
+import java.util.*;
+
+public class Budget {
+    protected double income;
+    protected Map<String, Double> categoryLimits;
+    protected List<Transaction> transactions;
+
+    public Budget(double income, Map<String, Double> categoryLimits) {
+        this.income = income;
+        this.categoryLimits = categoryLimits;
+        this.transactions = new ArrayList<>();
+    }
+
+    // Encapsulation: expenses can only be added, not edited directly
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    protected double getTotalExpenses() {
+        double total = 0;
+        for (Transaction t : transactions) {
+            if (t.getType().equalsIgnoreCase("EXPENSE")) {
+                total += t.getAmount();
+            }
+        }
+        return total;
+    }
+
+    // Operator usage: net savings
+    public double calculateNetSavings() {
+        return income - getTotalExpenses();
+    }
+}
