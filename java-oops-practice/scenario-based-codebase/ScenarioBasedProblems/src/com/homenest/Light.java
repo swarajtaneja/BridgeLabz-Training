41975f12fe022897727073c3ee672e413f3c9746
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-13 14:55:58 +0530

    [Swaraj] : Added light file in scenarionbased homenest

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/homenest/Light.java
@@ -0,0 +1,14 @@
+package com.homenest;
+
+public class Light extends Device {
+
+    public Light(String deviceId) {
+        super(deviceId, 0.06);
+    }
+
+    @Override
+    public void reset() {
+        turnOff();
+        System.out.println("Light " + deviceId + " reset: brightness set to default.");
+    }
+}
