9d0723ee598f1df5ead8714fc38fb6821ecc7d7e
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-13 14:56:16 +0530

    [Swaraj] : Added thermostat file in scenarionbased homenest

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/homenest/Thermostat.java
@@ -0,0 +1,17 @@
+package com.homenest;
+
+public class Thermostat extends Device {
+
+    private int temperature;
+
+    public Thermostat(String deviceId) {
+        super(deviceId, 0.20);
+        this.temperature = 24;
+    }
+
+    @Override
+    public void reset() {
+        temperature = 24;
+        System.out.println("Thermostat " + deviceId + " reset: temperature set to 24°C.");
+    }
+}
