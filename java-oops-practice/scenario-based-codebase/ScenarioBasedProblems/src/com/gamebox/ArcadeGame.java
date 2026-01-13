d9b699d113bc49ae95b834aa0bb71190e73fd455
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-13 17:04:26 +0530

    [Swaraj] : Added arcadegame file in scenarionbased gamebox

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/gamebox/ArcadeGame.java
@@ -0,0 +1,13 @@
+package com.gamebox;
+
+public class ArcadeGame extends Game {
+
+    public ArcadeGame(String title, double price, double rating) {
+        super(title, "Arcade", price, rating);
+    }
+
+    @Override
+    public void playDemo() {
+        System.out.println("Arcade Demo: Fast-paced action for 2 minutes!");
+    }
+}
