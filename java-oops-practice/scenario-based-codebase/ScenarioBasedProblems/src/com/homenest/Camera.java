2fe2e6c264017a75438989b81f5c3fcbc7279396
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-13 14:54:26 +0530

    [Swaraj] : Added camera file in scenarionbased homenest

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/homenest/Camera.java
@@ -0,0 +1,14 @@
+package com.homenest;
+
+public class Camera extends Device {
+
+    public Camera(String deviceId) {
+        super(deviceId, 0.15);
+    }
+
+    @Override
+    public void reset() {
+        turnOff();
+        System.out.println("Camera " + deviceId + " reset: recalibrating motion sensors.");
+    }
+}
