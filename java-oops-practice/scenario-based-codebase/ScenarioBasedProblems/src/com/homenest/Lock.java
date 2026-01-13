7710d925b3563eb2e1d72f3539195b6f05b7683d
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-13 14:57:57 +0530

    [Swaraj] : Added lock file in scenarionbased homenest

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/homenest/Lock.java
@@ -0,0 +1,14 @@
+package com.homenest;
+
+public class Lock extends Device {
+
+    public Lock(String deviceId) {
+        super(deviceId, 0.02);
+    }
+
+    @Override
+    public void reset() {
+        turnOff();
+        System.out.println("Lock " + deviceId + " reset: security codes refreshed.");
+    }
+}
