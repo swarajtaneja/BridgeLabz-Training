5d7e577cb923ef4bf57896668cd2a3dc58ab49ff
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-15 10:36:44 +0530

    [Swaraj] : Added main file in scenarionbased parcel tracker

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/parceltracker/Main.java
@@ -0,0 +1,28 @@
+package com.parceltracker;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		  Tracker tracker = new Tracker();
+
+	        tracker.insertStage("Packed");
+	        tracker.insertStage("Shipped");
+	        tracker.insertStage("In Transit");
+	        tracker.insertStage("Delivered");
+
+	        tracker.trackParcel();
+	        System.out.println();
+
+	        // Add custom checkpoint
+	        tracker.addCustomStage("Shipped", "Customs Check");
+	        tracker.trackParcel();
+	        System.out.println();
+
+	        // Check parcel status
+	        tracker.checkForLoss();
+	}
+
+}
