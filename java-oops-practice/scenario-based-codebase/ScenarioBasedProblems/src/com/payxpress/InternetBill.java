f6c72d93b322f152013919e0f5576b35a193ae3e
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-15 10:15:33 +0530

    [Swaraj] : Added internet bill file in scenarionbased payxpress

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/payxpress/InternetBill.java
@@ -0,0 +1,17 @@
+package com.payxpress;
+
+import java.time.LocalDate;
+
+public class InternetBill extends Bill {
+
+    public InternetBill(double amount, LocalDate dueDate) {
+        super("Internet", amount, dueDate);
+    }
+
+    @Override
+    public void sendReminder() {
+        if (!isPaid()) {
+            System.out.println("🌐 Internet Bill Reminder: Avoid service suspension.");
+        }
+    }
+}
