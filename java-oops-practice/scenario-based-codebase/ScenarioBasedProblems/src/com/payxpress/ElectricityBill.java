040796c1252ecd6f9824091af1dbce69dceb02c8
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-15 10:15:16 +0530

    [Swaraj] : Added electricity bill file in scenarionbased payxpress

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/payxpress/ElectricityBill.java
@@ -0,0 +1,17 @@
+package com.payxpress;
+
+import java.time.LocalDate;
+
+public class ElectricityBill extends Bill {
+
+    public ElectricityBill(double amount, LocalDate dueDate) {
+        super("Electricity", amount, dueDate);
+    }
+
+    @Override
+    public void sendReminder() {
+        if (!isPaid()) {
+            System.out.println("⚡ Electricity Bill Reminder: Pay before power disconnection!");
+        }
+    }
+}
