67d677573ed3263047e0a37ddf8714b53f213f3b
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-15 10:16:23 +0530

    [Swaraj] : Added water bill file in scenarionbased payxpress

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/payxpress/WaterBill.java
@@ -0,0 +1,17 @@
+package com.payxpress;
+
+import java.time.LocalDate;
+
+public class WaterBill extends Bill {
+
+    public WaterBill(double amount, LocalDate dueDate) {
+        super("Water", amount, dueDate);
+    }
+
+    @Override
+    public void sendReminder() {
+        if (!isPaid()) {
+            System.out.println("💧 Water Bill Reminder: Ensure uninterrupted water supply.");
+        }
+    }
+}
