bf4e2d6002e05cdbcb2be3a88597b982ad402385
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-15 11:36:00 +0530

    [Swaraj] : Added swapnode file in dsa leetcode day-2

---
+++ dsa-practice/leetcode-codebase/day-2/SwapNode.java
@@ -0,0 +1,18 @@
+class SwapNode {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+
+        while (prev.next != null && prev.next.next != null) {
+            ListNode first = prev.next;
+            ListNode second = first.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            prev = first;
+        }
+        return dummy.next;
+    }
+}
