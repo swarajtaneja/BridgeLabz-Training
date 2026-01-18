6067d7b7235c5064e5e4130ab4f379c7c71f05f4
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-18 23:30:26 +0530

    [Swaraj] : Added main file in universitycourse

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/generics/universitycourse/Main.java
@@ -0,0 +1,35 @@
+package com.generics.universitycourse;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Course<ExamCourse> math =
+                new Course<>("MTH101", "Calculus", new ExamCourse());
+
+        Course<AssignmentCourse> cs =
+                new Course<>("CSE201", "Data Structures", new AssignmentCourse());
+
+        Course<ResearchCourse> phd =
+                new Course<>("RES501", "AI Research", new ResearchCourse());
+
+        Department science = new Department("Science & Technology");
+        science.addCourse(math);
+        science.addCourse(cs);
+        science.addCourse(phd);
+
+        science.displayCourses();
+
+        // Wildcard usage
+        List<CourseType> courseTypes = Arrays.asList(
+                new ExamCourse(),
+                new AssignmentCourse(),
+                new ResearchCourse()
+        );
+
+        System.out.println("\nAvailable Course Types:");
+        CourseUtils.displayCourses(courseTypes);
+    }
+}
