204744b835cc7032bd3f8bc0136edcd520fcfa84
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-18 23:22:53 +0530

    [Swaraj] : Added resume file in screeningsystem

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/generics/sreeningsystem/Resume.java
@@ -0,0 +1,19 @@
+package com.generics.sreeningsystem;
+
+public class Resume<T extends JobRole> {
+
+    private T candidate;
+
+    public Resume(T candidate) {
+        this.candidate = candidate;
+    }
+
+    public T getCandidate() {
+        return candidate;
+    }
+
+    public void displayResume() {
+        System.out.println("Processing Resume: " + candidate);
+    }
+}
+
