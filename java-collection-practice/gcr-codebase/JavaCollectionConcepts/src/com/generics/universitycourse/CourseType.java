04d300edee544832d8fe39a99a4df9ce53951d1c
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-18 23:28:39 +0530

    [Swaraj] : Added course type file in universitycourse

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/generics/universitycourse/CourseType.java
@@ -0,0 +1,21 @@
+package com.generics.universitycourse;
+
+public abstract class CourseType {
+
+    private String evaluationMethod;
+
+    public CourseType(String evaluationMethod) {
+        this.evaluationMethod = evaluationMethod;
+    }
+
+    public String getEvaluationMethod() {
+        return evaluationMethod;
+    }
+
+    public abstract String getTypeName();
+
+    @Override
+    public String toString() {
+        return getTypeName() + " (" + evaluationMethod + ")";
+    }
+}
