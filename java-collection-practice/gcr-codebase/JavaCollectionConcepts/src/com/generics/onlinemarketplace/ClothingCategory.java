373b9e60796534a09b82d646d1dd01e20940d06a
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-18 23:07:47 +0530

    [Swaraj] : Added clothing category file in online market place

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/generics/onlinemarketplace/ClothingCategory.java
@@ -0,0 +1,8 @@
+package com.generics.onlinemarketplace;
+
+public class ClothingCategory implements Category {
+    @Override
+    public String getCategoryName() {
+        return "Clothing";
+    }
+}
