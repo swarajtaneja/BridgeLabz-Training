026bba2e55038d96ca8340a783c6f6564466f0d4
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-18 22:26:37 +0530

    [Swaraj] : Added sensor file in senario based cropmonitor

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/cropmonitor/Sensor.java
@@ -0,0 +1,12 @@
+package com.cropmonitor;
+
+public class Sensor {
+	
+	int timeStamp;
+	double temp;
+	
+	Sensor(int timeStamp, double temp){
+		this.timeStamp = timeStamp;
+		this.temp = temp;
+	}
+}
