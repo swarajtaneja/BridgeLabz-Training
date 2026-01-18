c9a8be610c9af2657938f4c53d894171fc942a6d
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-18 23:29:48 +0530

    [Swaraj] : Added examcourse file in universitycourse

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/generics/universitycourse/ExamCourse.java
@@ -0,0 +1,13 @@
+package com.generics.universitycourse;
+
+public class ExamCourse extends CourseType {
+
+    public ExamCourse() {
+        super("Written Exam");
+    }
+
+    @Override
+    public String getTypeName() {
+        return "Exam-Based Course";
+    }
+}
