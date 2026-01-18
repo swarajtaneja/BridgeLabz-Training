73d13cb0957acf87af9085beb7111bddd3e1a933
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-18 23:10:52 +0530

    [Swaraj] : Added furniture file in smart warehouse

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/generics/smartwarehouse/Furniture.java
@@ -0,0 +1,12 @@
+package com.generics.smartwarehouse;
+
+public class Furniture extends WareHouseItem {
+    public Furniture(String name, int quantity) {
+        super(name, quantity);
+    }
+
+    @Override
+    public String getCategory() {
+        return "Furniture";
+    }
+}
