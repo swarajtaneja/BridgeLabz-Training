16b391555dc486ea8493083a3cc1d07f4585ff1a
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-18 23:10:37 +0530

    [Swaraj] : Added electronics file in smart warehouse

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/generics/smartwarehouse/Electronics.java
@@ -0,0 +1,12 @@
+package com.generics.smartwarehouse;
+
+public class Electronics extends WareHouseItem {
+    public Electronics(String name, int quantity) {
+        super(name, quantity);
+    }
+
+    @Override
+    public String getCategory() {
+        return "Electronics";
+    }
+}
