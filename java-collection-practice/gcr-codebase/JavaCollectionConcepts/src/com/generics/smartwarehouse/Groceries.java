81e558bf1bebeb3ec4ef8de73067fac28e5c0486
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-18 23:11:11 +0530

    [Swaraj] : Added groceries file in smart warehouse

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/generics/smartwarehouse/Groceries.java
@@ -0,0 +1,12 @@
+package com.generics.smartwarehouse;
+
+public class Groceries extends WareHouseItem {
+    public Groceries(String name, int quantity) {
+        super(name, quantity);
+    }
+
+    @Override
+    public String getCategory() {
+        return "Groceries";
+    }
+}
