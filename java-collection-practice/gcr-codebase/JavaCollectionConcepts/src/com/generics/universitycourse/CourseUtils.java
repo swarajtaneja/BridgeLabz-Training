473c3c911581050826e3cbda43f44839f521a8ab
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-18 23:29:16 +0530

    [Swaraj] : Added course utils file in universitycourse

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/generics/universitycourse/CourseUtils.java
@@ -0,0 +1,12 @@
+package com.generics.universitycourse;
+
+import java.util.List;
+
+public class CourseUtils {
+
+    public static void displayCourses(List<? extends CourseType> courseTypes) {
+        for (CourseType type : courseTypes) {
+            System.out.println(type);
+        }
+    }
+}
