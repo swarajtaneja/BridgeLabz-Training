b1605c26842ecaab9378c0774fd4079faec92919
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-21 14:57:56 +0530

    [Swaraj] : Added stall file in scenariobased foodfest

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/foodfest/Stall.java
@@ -0,0 +1,11 @@
+package com.foodfest;
+
+public class Stall {
+    String stallName;
+    int footfall;
+
+    public Stall(String stallName, int footfall) {
+        this.stallName = stallName;
+        this.footfall = footfall;
+    }
+}
