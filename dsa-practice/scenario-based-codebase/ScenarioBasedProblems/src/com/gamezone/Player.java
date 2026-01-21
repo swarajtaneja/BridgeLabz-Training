689334106cb92480a64429942e14b2693268e87d
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-21 15:02:59 +0530

    [Swaraj] : Added player file in scenariobased gamezone

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/gamezone/Player.java
@@ -0,0 +1,11 @@
+package com.gamezone;
+
+public class Player {
+    String playerName;
+    int score;
+
+    public Player(String playerName, int score) {
+        this.playerName = playerName;
+        this.score = score;
+    }
+}
