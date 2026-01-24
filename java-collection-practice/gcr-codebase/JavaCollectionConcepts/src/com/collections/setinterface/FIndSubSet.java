739a8d2cdd3c49628d965c3f5e30067225d7bce6
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-24 15:43:57 +0530

    [Swaraj] : Added findsubset file in collections

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/collections/setinterface/FIndSubSet.java
@@ -0,0 +1,13 @@
+package com.collections.setinterface;
+import java.util.*;
+public class FIndSubSet {
+	public static void main(String[] args) {
+
+		Set<Integer> set1 = new HashSet<>(Arrays.asList(2, 3));
+		Set<Integer> set2 = new HashSet<>(Arrays.asList(1, 2, 3, 4));
+		
+		boolean isSubset = set2.containsAll(set1);
+		System.out.println(isSubset);
+	}
+
+}
