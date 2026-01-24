7f091f8b6cfc6d1baeba424ec1036c1b501b1705
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-24 15:40:46 +0530

    [Swaraj] : Added main and cirularbuffer file in collections circularbuffer

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/collections/queueinterface/circularbuffer/Main.java
@@ -0,0 +1,18 @@
+package com.collections.queueinterface.circularbuffer;
+
+public class Main{
+
+	public static void main(String[] args) {
+
+		CircularBuffer cb = new CircularBuffer(3);
+
+        cb.insert(1);
+        cb.insert(2);
+        cb.insert(3);
+        cb.display();
+
+        cb.insert(4);
+        cb.display();
+	}
+
+}
