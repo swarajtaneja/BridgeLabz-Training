5b8c27aec92e2b3ce756f07ea89c1a60a00d1a16
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-24 15:37:46 +0530

    [Swaraj] : Added employee file in collections groupobjects

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/collections/mapinterface/groupobjects/Employee.java
@@ -0,0 +1,17 @@
+package com.collections.mapinterface.groupobjects;
+
+public class Employee {
+
+	String name;
+    String department;
+
+    public Employee(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
