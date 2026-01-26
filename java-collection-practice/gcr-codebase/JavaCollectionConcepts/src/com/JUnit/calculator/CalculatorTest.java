6e61589701b853696bf2992819a97ec7177888f7
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 22:07:29 +0530

    [Swaraj] : Added calculator and calculatortest file in junit

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/JUnit/calculator/CalculatorTest.java
@@ -0,0 +1,37 @@
+package com.JUnit.calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        assertEquals(7, calc.add(3, 4));
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calc = new Calculator();
+        assertEquals(2, calc.subtract(5, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        Calculator calc = new Calculator();
+        assertEquals(15, calc.multiply(3, 5));
+    }
+
+    @Test
+    public void testDivide() {
+        Calculator calc = new Calculator();
+        assertEquals(2, calc.divide(6, 3));
+    }
+
+    @Test
+    public void testDivideByZero() {
+        Calculator calc = new Calculator();
+        assertThrows(ArithmeticException.class, () -> calc.divide(5, 0));
+    }
+}
