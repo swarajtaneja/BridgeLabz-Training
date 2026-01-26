8ff110c22e7975861c7607d286fbdcc57783a11f
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 22:25:44 +0530

    [Swaraj] : Added IP file in regex

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/regex/IP.java
@@ -0,0 +1,7 @@
+package com.regex;
+public class IP {
+    public static void main(String[] args) {
+        String s = "192.168.1.1";
+        System.out.println(s.matches("((25[0-5]|2[0-4]\\d|[01]?\\d\\d?)\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d\\d?)"));
+    }
+}
