ad1cdac1c5e02bf1c45f34f0d05fbd1c53242945
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 22:27:14 +0530

    [Swaraj] : Added SSN file in regex

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/regex/SSN.java
@@ -0,0 +1,7 @@
+package com.regex;
+public class SSN {
+    public static void main(String[] args) {
+        String s = "123-45-6789";
+        System.out.println(s.matches("\\d{3}-\\d{2}-\\d{4}"));
+    }
+}
