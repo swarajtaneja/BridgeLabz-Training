6e59adca9e917bc0721a62c8e960b2561d6bb473
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 22:07:59 +0530

    [Swaraj] : Added dateformatter and dateformattertest file in junit

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/JUnit/DateFormatter/DateFormatterTest.java
@@ -0,0 +1,18 @@
+package com.JUnit.DateFormatter;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class DateFormatterTest {
+
+    @Test
+    public void testValidDate() {
+        DateFormatter df = new DateFormatter();
+        assertEquals("25-12-2023", df.formatDate("2023-12-25"));
+    }
+
+    @Test
+    public void testInvalidDate() {
+        DateFormatter df = new DateFormatter();
+        assertThrows(IllegalArgumentException.class, () -> df.formatDate("12-25-2023"));
+    }
+}
