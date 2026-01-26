3a43f6b1715948dda012382825a81584f10fffdd
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 19:56:40 +0530

    [Swaraj] : Added UpperToLower file in streams

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/streams/UpperToLower.java
@@ -0,0 +1,21 @@
+package com.streams;
+import java.io.*;
+
+public class UpperToLower {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+             BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt"))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                bw.write(line.toLowerCase());
+                bw.newLine();
+            }
+
+            System.out.println("Conversion completed.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
