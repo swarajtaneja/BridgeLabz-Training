63da2c3f3fe37e03d1721f3b518e2d68470b0289
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 18:58:22 +0530

    [Swaraj] : Added banktransaction and insufficient balanceexception file in exception banktransactionsystem

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/exception/baktransactionsystem/InsufficientBalanceException.java
@@ -0,0 +1,8 @@
+package com.exception.baktransactionsystem;
+
+public class InsufficientBalanceException extends Exception {
+
+	public InsufficientBalanceException(String message) {
+		super(message);
+	}
+}
