f40baa159f9897cf695bdc12811437a630f8d434
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 22:09:49 +0530

    [Swaraj] : Added eventchecker and event checker test file in junit

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/JUnit/parametrizedtests/EvenChecker.java
@@ -0,0 +1,6 @@
+package com.JUnit.parametrizedtests;
+public class EvenChecker {
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/JUnit/parametrizedtests/EvenCheckerTest.java
@@ -0,0 +1,18 @@
+package com.JUnit.parametrizedtests;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class EvenCheckerTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 7, 9})
+    public void testIsEven(int number) {
+        EvenChecker checker = new EvenChecker();
+        if (number % 2 == 0) {
+            assertTrue(checker.isEven(number));
+        } else {
+            assertFalse(checker.isEven(number));
+        }
+    }
+}
