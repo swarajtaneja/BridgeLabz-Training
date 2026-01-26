94814538b7a3ebd26e6379d08500e90e867a8d33
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 22:06:50 +0530

    [Swaraj] : Added bank account and bank account test file in junit

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/JUnit/Banking/BankAccountTest.java
@@ -0,0 +1,27 @@
+package com.JUnit.Banking;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class BankAccountTest {
+
+    @Test
+    public void testDeposit() {
+        BankAccount account = new BankAccount();
+        account.deposit(100);
+        assertEquals(100, account.getBalance());
+    }
+
+    @Test
+    public void testWithdraw() {
+        BankAccount account = new BankAccount();
+        account.deposit(200);
+        account.withdraw(50);
+        assertEquals(150, account.getBalance());
+    }
+
+    @Test
+    public void testWithdrawInsufficientFunds() {
+        BankAccount account = new BankAccount();
+        assertThrows(IllegalArgumentException.class, () -> account.withdraw(50));
+    }
+}
