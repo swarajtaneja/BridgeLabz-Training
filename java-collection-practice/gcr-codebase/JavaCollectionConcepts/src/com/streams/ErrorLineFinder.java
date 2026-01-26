c0c75171261039a5d7c92a04e15dd6cdfe08d8a8
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 19:54:46 +0530

    [Swaraj] : Added ErrorLineFinder.java file in streams

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/streams/ErrorLineFinder.java
@@ -0,0 +1,17 @@
+package com.streams;
+import java.io.*;
+
+public class ErrorLineFinder {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("large.log"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.toLowerCase().contains("error")) {
+                    System.out.println(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
