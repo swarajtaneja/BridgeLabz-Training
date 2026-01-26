e558c5ed29f9ca54b4a28d1ba95e315228110bfa
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 22:27:26 +0530

    [Swaraj] : Added username file in regex

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/regex/Username.java
@@ -0,0 +1,7 @@
+package com.regex;
+public class Username {
+    public static void main(String[] args) {
+        String s = "user_123";
+        System.out.println(s.matches("^[A-Za-z][A-Za-z0-9_]{4,14}$"));
+    }
+}
