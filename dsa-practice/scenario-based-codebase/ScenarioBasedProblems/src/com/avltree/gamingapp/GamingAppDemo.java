4b682af71a44beeeda74e4e69be7822783b4c378
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 22:48:00 +0530

    [Swaraj] : Added avl gaming, gaming app demo and player node file in avl tree gaming app

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/avltree/gamingapp/GamingAppDemo.java
@@ -0,0 +1,27 @@
+package com.avltree.gamingapp;
+import java.util.*;
+
+public class GamingAppDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AVLGaming game = new AVLGaming();
+
+        // 🔹 Scenario 1: Insert / Update Players
+        game.root = game.insert(game.root, "Ravi", 1200);
+        game.root = game.insert(game.root, "Ritesh", 1500);
+        game.root = game.insert(game.root, "Swaraj", 900);
+        game.root = game.insert(game.root, "Mukesh", 1800);
+        
+        List<PlayerNode> topPlayers = new ArrayList<>();
+        game.getTopPlayers(game.root, topPlayers);
+        
+        for (int i = 0; i < topPlayers.size(); i++) {
+            PlayerNode p = topPlayers.get(i);
+            System.out.println((i + 1) + ". " + p.name + " - " + p.points + " pts");
+        }
+        
+	}
+
+}
