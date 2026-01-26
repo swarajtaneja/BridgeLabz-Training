60edff43e3865cf8282c4a117125b5f91760635d
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 22:51:49 +0530

    [Swaraj] : Added student, student bst and university record demo file in bst university record system

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/binarysearchtree/universityrecordsystem/Student.java
@@ -0,0 +1,14 @@
+package com.binarysearchtree.universityrecordsystem;
+
+public class Student {   // node
+
+	public int rollNo;
+	public String name;
+	Student left, right;
+	
+	public Student(int rollNo, String name) {
+		this.name = name;
+		this.rollNo = rollNo;
+		left = right = null;
+	}
+}
