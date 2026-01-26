97a5ae0c322fa828d829e5ec469a527adad64e45
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 22:51:04 +0530

    [Swaraj] : Added music app demo, song bst and song node file in bst music app

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/binarysearchtree/musicapp/SongNode.java
@@ -0,0 +1,14 @@
+package com.binarysearchtree.musicapp;
+
+public class SongNode {
+
+	int trackId;
+	String title;
+	SongNode left, right;
+	
+	public SongNode(int trackId, String title) {
+		this.trackId = trackId;
+		this.title = title;
+		left = right = null;
+	}
+}
