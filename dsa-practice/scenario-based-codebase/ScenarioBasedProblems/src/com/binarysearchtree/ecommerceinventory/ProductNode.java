05e119d7b6a51842b2515edfef117fe06016580a
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 22:50:30 +0530

    [Swaraj] : Added ecommerce invendemo , product bst and product node file in bst ecommerceinventory

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/binarysearchtree/ecommerceinventory/ProductNode.java
@@ -0,0 +1,18 @@
+package com.binarysearchtree.ecommerceinventory;
+
+public class ProductNode {
+
+	int sku;
+	String name;
+	double price;
+	ProductNode left, right;
+	
+	public ProductNode(int sku, String name, double price) {
+		this.sku = sku;
+		this.name = name;
+		this.price = price;
+		left = right = null;
+	}
+	
+	
+}
