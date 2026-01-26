d82ecf7b44aabc2f1b20182a32a7ee6849842096
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-26 19:56:02 +0530

    [Swaraj] : Added PipedStreamDemo file in streams

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/streams/PipedStreamDemo.java
@@ -0,0 +1,32 @@
+package com.streams;
+import java.io.*;
+
+public class PipedStreamDemo {
+    public static void main(String[] args) throws IOException {
+        PipedOutputStream pos = new PipedOutputStream();
+        PipedInputStream pis = new PipedInputStream(pos);
+
+        Thread writer = new Thread(() -> {
+            try {
+                pos.write("Hello from writer thread".getBytes());
+                pos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread reader = new Thread(() -> {
+            try {
+                int data;
+                while ((data = pis.read()) != -1) {
+                    System.out.print((char) data);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        writer.start();
+        reader.start();
+    }
+}
