cc2da67f0ae45848bdb73583ff019e0788856b07
swarajtaneja <swaraj97713473431@gmail.com>
2026-01-28 00:29:24 +0530

    [Swaraj] : Added largecsv file in readlargecsv

---
+++ java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/com/csvdata/readlargecsv/LargeCSV.java
@@ -0,0 +1,17 @@
+package com.csvdata.readlargecsv;
+import java.io.*;
+
+public class LargeCSV {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("large.csv"));
+        int count = 0;
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            count++;
+            if (count % 100 == 0)
+                System.out.println("Processed: " + count);
+        }
+        br.close();
+    }
+}
