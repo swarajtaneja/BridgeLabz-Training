925d118bb5136b1a2d5aeb1b5fafcd98ef174862
swarajtaneja <swaraj97713473431@gmail.com>
2026-02-02 13:22:12 +0530

    [Swaraj] : Added lessthanfifty file in scenariobased javastreams

---
+++ java8-programming-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/javastreams/LessThanFifty.java
@@ -0,0 +1,19 @@
+package com.javastreams;
+import java.util.*;
+
+public class LessThanFifty {
+	public static void lessThanFifty(Student[] student) {
+		
+		Arrays.stream(student).filter(stu -> stu.rank<50).forEach(System.out::println);
+	}
+	public static void main(String[] args) {
+		
+		Student[] student = { new Student("Swaraj Kumar",111,"Swaraj",22,"M","Katni",1,"6203565963"),
+				   new Student("daku maharaj",98,"daku",22,"M","Bhopal",2,"8334897238"),
+				   new Student("Shriyansh",83,"Shriyansh",21,"M","Sagar",3,"8334897435"),
+				   new Student("xyz iad",78,"xyzd",26,"F","hfjksh",53,"83348972332"),
+				   new Student("jhs hjs", 67,"jhs",38,"F","hskf",49,"8334897254")};
+		
+		lessThanFifty(student);
+	}
+}
