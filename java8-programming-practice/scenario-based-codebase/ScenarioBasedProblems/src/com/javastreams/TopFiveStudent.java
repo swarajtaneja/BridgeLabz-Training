16b3ea10cc1d0b7a3472fb6ee6e1b32f55b933ab
swarajtaneja <swaraj97713473431@gmail.com>
2026-02-02 13:28:12 +0530

    [Swaraj] : Added topfivestudent file in scenariobased javastreams

---
+++ java8-programming-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/javastreams/TopFiveStudent.java
@@ -0,0 +1,15 @@
+package com.javastreams;
+import java.util.*;
+
+public class TopFiveStudent {
+	public static void main(String[] args) {
+		List<Student> student = List.of(new Student("Swaraj Kumar",111,"Swaraj",22,"M","Katni",1,"6203565963"),
+				   new Student("daku maharaj",98,"daku",22,"M","Bhopal",2,"8334897238"),
+				   new Student("Shriyansh",83,"Shriyansh",21,"M","Sagar",3,"8334897435"),
+				   new Student("xyz iad",78,"xyzd",26,"F","hfjksh",53,"83348972332"),
+				   new Student("jhs hjs", 67,"jhs",38,"F","hskf",49,"8334897254"));
+		
+		student.stream().sorted(Comparator.comparingInt(s->s.rank))
+				.limit(5).forEach(System.out::println);
+	}
+}
